3bbbc8a2e1a296db5b3f840fa20d2fe39b3d043d
Mateus Roveda <mateus.roveda@universo.univates.br>
2024-07-05 16:50:59 -0300

    Atualiza boilerplate

--- boilerplate-api/app.ts
+++ boilerplate-api/app.ts
@@ -1,7 +1,11 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
 const port: number = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response): Response => {
   return res.status(200).json({ message: 'Olá mundo!' });
