0c21c3961215ee18b95e1abbd3652ba6a7c2ba01
Mateus Roveda <mateus.roveda@universo.univates.br>
2024-07-19 14:37:05 -0300

    Adiciona log de requisições

--- api-bebida/src/app.ts
+++ api-bebida/src/app.ts
@@ -1,3 +1,4 @@
+import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import BebidaController from './Controllers/BebidaController';
 import Bebida from './Models/Bebida';
@@ -8,6 +9,10 @@ let copa: BebidaController = new BebidaController();
 
 app.use(cors());
 app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date()}] ${req.ip} ${req.method} ${req.url}`);
+  next();
+});
 
 app.get('/bebidas', (req: Request, res: Response): Response => {
   const bebidas = copa.listarTodos();
